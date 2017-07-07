e5c8a080d79ed076b4f3f8e2792c67928a6ef11c
Mohammed Elsammak <mohammed.elsammak@modeso.ch>
2017-07-07 13:45:28 +0200

    Create project structure.

---
+++ Euroleague-Graduate/Euroleague-Graduate/ModelView/AbstractViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  AbstractViewModel.swift
+//  Euroleague-Graduate
+//
+//  Created by Mohammed Elsammak on 7/7/17.
+//  Copyright © 2017 Modeso. All rights reserved.
+//
+
+import Foundation
---
+++ Euroleague-Graduate/Euroleague-Graduate/View/AbstractViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AbstractViewController.swift
+//  Euroleague-Graduate
+//
+//  Created by Mohammed Elsammak on 7/7/17.
+//  Copyright © 2017 Modeso. All rights reserved.
+//
+
+import UIKit
+
+class AbstractViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
